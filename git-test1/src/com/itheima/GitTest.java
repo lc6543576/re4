bf7d1d6c27fbb89e0a6a4f8337b4a1fc43fe243f
lc <lc6543576@163.com>
2019-09-09 19:56:54 +0800

    xiugai

--- git-test1/src/com/itheima/GitTest.java
+++ git-test1/src/com/itheima/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("nihao");
         System.out.println("nihao");
+        System.out.println("nihao1");
+        System.out.println("nihao1");
     }
 }
