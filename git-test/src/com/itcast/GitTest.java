0354084699a849714f49feb2f30f00c4b7541b34
lc <lc6543576@163.com>
2019-09-09 19:00:18 +0800

    tianjia一个工程

---
+++ git-test/src/com/itcast/GitTest.java
@@ -0,0 +1,9 @@
+package com.itcast;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("你好啊");
+        System.out.println("你好啊");
+        System.out.println("你好啊");
+    }
+}
